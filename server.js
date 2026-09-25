637ca3c4f9f9c03ec15139c1675529a5272d3953
samehabouelazm-cloud <samehabouelazm@gmail.com>
2026-09-26 01:53:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -122,11 +122,13 @@ app.put('/api/orders/:id/status', (req, res) => {
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'customer_app', 'index.html')));
 
 app.listen(PORT, () => console.log(`🚀 READY OS Server running on port: ${PORT}`));
-// مسار حذف الكابتن برقم الـ ID
+// مسار حذف كابتن بواسطة ID
 app.delete('/api/drivers/:id', (req, res) => {
     const { id } = req.params;
     const initialLength = global.drivers.length;
-    global.drivers = global.drivers.filter(d => d.id != id);
+    
+    // التصفية لمطابقة ID سواء كان نوعه String أو Number
+    global.drivers = global.drivers.filter(d => String(d.id) !== String(id));
 
     if (global.drivers.length === initialLength) {
         return res.status(404).json({ error: 'الكابتن غير موجود' });
